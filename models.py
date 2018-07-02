d8478623a47a12279cee50e9b8e8fd2b8d4d8439
MariaRoda <xirimoia86@gmail.com>
2018-07-02 13:39:51 +0200

    Conector base de datos

--- models.py
+++ models.py
@@ -11,7 +11,7 @@ from random import randint
 
 app = Flask(__name__)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = getenv('SQLALCHEMY_DATABASE_URI')
+app.config['SQLALCHEMY_DATABASE_URI'] = getenv('DATABASE_URL')
 app.config['SECRET_KEY'] = getenv('SECRET_KEY')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
